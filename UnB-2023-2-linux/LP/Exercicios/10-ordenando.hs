3076fe57c90443fc3dd41623905c1a5e190e64f5
Danillo <danillodca@gmail.com>
2023-09-27 21:51:17 -0300

    atualizando linux

---
+++ UnB-2023-2-linux/LP/Exercicios/10-ordenando.hs
@@ -0,0 +1,24 @@
+l1::[Int]
+l1 = [3,4,2,5,9,2]
+
+myFst :: [Int] -> Int
+myFst [x] = x
+myFst (x:xs) = x
+
+getMenor :: [Int] -> Int
+getMenor [x] = x
+getMenor (x:xs) | x < getMenor xs = x
+                | otherwise = getMenor xs
+
+removeMenor :: [Int] -> [Int]
+removeMenor [x] = []
+removeMenor (x:xs)  | x == getMenor (x:xs) = xs
+                    | otherwise = x: removeMenor xs
+
+aux :: [Int] -> [Int] -> [Int]
+aux listaOrdenada [] = listaOrdenada
+aux listaOdenada (x:xs) = aux ( listaOdenada ++ [getMenor (x:xs)]) (removeMenor (x:xs))                   
+
+mySort :: [Int] -> [Int]
+mySort [] = []
+mySort (x:xs) = aux [] (x:xs)
